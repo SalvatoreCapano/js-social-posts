59a7af9dfc00e4910e5ab1ee1cc09ea39a5c4c83
Salvatore Capano <capano_salvatore@libero.it>
2023-01-19 11:15:38 +0100

    Fix: aggiunto ID ai post stampati

--- main.js
+++ main.js
@@ -103,13 +103,13 @@ posts.forEach((postData, index) => {
     <div class="post__footer">
         <div class="likes js-likes">
             <div class="likes__cta">
-                <a class="like-button  js-like-button" href="##" data-postid="1">
+                <a class="like-button  js-like-button" href="##" data-postid="${postData.id}">
                     <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                     <span class="like-button__label">Mi Piace</span>
                 </a>
             </div>
             <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">${postData.likes}</b> persone
+                Piace a <b id="like-counter-${postData.id}" class="js-likes-counter">${postData.likes}</b> persone
             </div>
         </div> 
     </div>       
